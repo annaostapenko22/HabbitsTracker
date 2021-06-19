d122dac994a8ce4a01a020d14d05144517d62509
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-06-19 19:19:19 +0300

    Последние изменения минимальны, добавлены outlets, кнопки. Вроде рабочий вариант. Можете собрать в кучу, хотя бы покажем, что в гит ходили, блуждали, но разобрались :)

--- TravelApp/AddHabitViewController.swift
+++ TravelApp/AddHabitViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class AddHabitViewController: UITableViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var saveHabitButton: UIBarButtonItem!
+    
+    @IBOutlet weak var newHabitTextField: UITextField!
+    @IBOutlet weak var descriptionHabitTextField: UITextField!
+    
+    @IBOutlet weak var datePickerHabit: UIDatePicker!
+    @IBOutlet weak var repeatSwitchHabit: UISwitch!
+    @IBOutlet weak var targetSwitchHabit: UISwitch!
+    
+    //MARK: - Public properties
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
     }
+    
+    
+    //MARK: - IBAction
+    @IBAction func pressedAddButton(_ sender: UIBarButtonItem) {
+      
+    }
+}
 
+//MARK: - UITextFieldDelegate
+extension AddHabitViewController: UITextFieldDelegate {
 
 }
+
+/*
+ 
+ не успел реализовать скрытие клавиатуры (тут обычный метод не прокатывает, тк у нас скролл есть.
+https://bestkora.com/IosDeveloper/dopolnenie-k-lektsii-8-kak-skryt-klaviaturu-posle-vvoda-teksta/
+С этим можете не заморачиваться, если не мешает, я как вернусь - доделаю до конца.
+ 
+Ну а остальное дело техники, думаю и без меня справитесь. Осталось по сути взять инфу с предыдущей дз. И я бы все же начал реализовывать все от таббарвьюконтроллера (можно у меня в домашке на гите посмотреть, и на занятии тоже отилчное решение есть).
+ 
+Удачи! При возможности подключусь сразу же :)
+ 
+ */
+
+
